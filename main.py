cb8562de7f1cfff6635dc677c91103d9df123615
MrElefantus <mr.elefantus@gmail.com>
2022-06-23 19:04:13 +0300

    tt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D, art3d
 from stl import mesh
 from mpl_toolkits import mplot3d
@@ -7,12 +8,19 @@ import numpy as np
 ax = plt.figure().add_subplot(projection='3d')
 
 #load cut data from file and show it
-x = []
-y = []
-z = []
-xn = []
-yn = []
-zn = []
+x, y, z = [],[],[]
+
+#only for visualization
+xn, yn, zn = [],[],[]
+
+LaserPointX = []
+LaserPointY = []
+LaserPointZ = []
+
+#initial point of laser
+LaserPointXInit = 0.0
+LaserPointYInit = -20.0
+LaserPointZInit = 30.0
 
 lines = [line.rstrip() for line in open('line.txt')]
 for line in lines:
@@ -23,19 +31,39 @@ for line in lines:
             yn.append(float(axisdata[1]))
             zn.append(float(axisdata[2]))
         else:
+            # Show line to cut
             x.append(float(axisdata[0]))
             y.append(float(axisdata[1]))
             z.append(float(axisdata[2]))
 
+            # Rotate aligner
+            print(axisdata[1])
+            # FIRST poinT of laser
+            LaserPointX.append(float(LaserPointXInit))
+            LaserPointY.append(float(LaserPointYInit))
+            LaserPointZ.append(float(LaserPointZInit))
+            #Rotate aligner
+
+            # END point or laser vesibular
+            LaserPointX.append(float(axisdata[0]))
+            LaserPointY.append(float(axisdata[1]))
+            LaserPointZ.append(float(axisdata[2]))
+            # END point or laser lingual
+
+# Точки разворота стола п
+print(*sorted(y)[:2])
+
 ax.plot(x, y, z, color='green')  # Plot positive contour curves
 ax.plot(xn, yn, zn, color='red')  # Plot negative contour curves
+ax.plot(LaserPointX, LaserPointY, LaserPointZ, color='blue')
+
 
 #show aligner model
 
-#aligner_stl = mesh.Mesh.from_file('stlfile.stl')
-#ax.add_collection3d(mplot3d.art3d.Poly3DCollection(aligner_stl.vectors))
-#scale = aligner_stl.points.flatten()
-#ax.auto_scale_xyz(scale, scale, scale)
+# aligner_stl = mesh.Mesh.from_file('stlfile.stl')
+# ax.add_collection3d(mplot3d.art3d.Poly3DCollection(aligner_stl.vectors))
+# scale = aligner_stl.points.flatten()
+# ax.auto_scale_xyz(scale, scale, scale)
 
 
 ax.set_xlabel("X label")
